ab8b51a44ddc86be07d57a0edbec8f361d2a4a55
Julian <julian.grande@disruptive-technologies.com>
2024-11-28 09:48:39 +0100

    feat: add `render-markdown` plugin to `nvim`

--- .config/lvim/lua/custom/plugins.lua
+++ .config/lvim/lua/custom/plugins.lua
@@ -194,5 +194,10 @@ lvim.plugins = {
         -- Configuration here, or leave empty to use defaults
       })
     end
+  },
+  {
+    'MeanderingProgrammer/render-markdown.nvim',
+    dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.nvim' }, -- if you use the mini.nvim suite
+    opts = {},
   }
 }
